2daac35dd0b036eefb0ea3d8a8eb06987dcbf396
avbravo <avbravo@avbravo-ThinkCentre-M70t-Gen-3>
2026-06-23 11:48:40 -0500

    autentificacion, acreditacion

---
+++ src/main/java/com/jettra/jwt/enumerations/TypeAcreditationOptions.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.jettra.jwt.enumerations;
+
+/**
+ *
+ * @author avbravo
+ */
+public enum TypeAcreditationOptions {
+    PAGE, MENU,SUBMENU,METHODS, CONTROLLER
+}
